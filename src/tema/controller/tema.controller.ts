d2f08c675997599352aba819bbed80f4b2bd4aaa
Leticia Fontes <leticia.abib95@gmail.com>
2025-06-25 16:53:32 -0300

    correcao da funcao criar tema

--- src/tema/controller/tema.controller.ts
+++ src/tema/controller/tema.controller.ts
@@ -27,7 +27,7 @@ export class TemaController {
     @Post()
     @HttpCode(HttpStatus.OK)
     create(@Body() tema: Tema):Promise<Tema>{
-        return this.temaService.update(tema);
+        return this.temaService.create(tema);
     }
 
     @Put()
