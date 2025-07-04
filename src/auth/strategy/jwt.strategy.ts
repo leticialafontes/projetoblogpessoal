1c1599af501e96a8e137a79beb7299a98e481848
Leticia Fontes <leticia.abib95@gmail.com>
2025-07-04 12:58:12 -0300

    feat: inclusão de sequencia de testes na classe usuario

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -9,7 +9,7 @@ export class JwtStrategy extends PassportStrategy(Strategy){
     constructor(){
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            ignoreExpiration: false,// aqui se o ignore estiver como true, o que aconteceria é, a pessoa quer acessar, o token dela esta desatualizado, mas mesmo assim ela vai passar, então é por isso que o ignore tem que esta como false, para ela ter que fazer o login novamente
+            ignoreExpiration: false,// aqui se o ignore estiver como true, o que aconteceria é, a pessoa quer acessar, o token dela esta desatualizado, mas mesmo assim ela vai passar, então é por isso que o ignore tem que esta como false, para ela ter que fazer o login novamente 
             secretOrKey: jwtConstants.secret
         })
     }
